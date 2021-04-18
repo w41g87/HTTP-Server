87514034529104e079b84b4c180b93ff37a0387a
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-04-18 19:37:28 -0400

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -54,9 +54,10 @@ string getContentByHeader(string str, string header) {
   cout << (int)str.at(0) << endl;
   cout << (int)str.at(1) << endl;
   cout << str.length() << endl;
-  cout << matchStart(str, string("\r\n")) << endl;
+  cout << (bool)matchStart(str, string("\r\n")) << endl;
   // sleep(1);
   if (matchStart(str, string("\r\n")) != 0) {
+    cout << "matched" << endl;
     return string("");
   } else if (matchStart(str, header)) {
     int pos = str.find(':') + 2;
