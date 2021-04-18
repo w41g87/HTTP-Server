2765c807dadc9c81a1537f80276e8339abe26244
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-04-18 19:23:25 -0400

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -53,6 +53,7 @@ bool validate (string const &str) {
 string getContentByHeader(string str, string header) {
   cout << (int)str.at(str.length() - 2) << endl;
   cout << (int)str.at(str.length() - 1) << endl;
+  cout << str.length() << endl;
   sleep(1);
   if (matchStart(str, string("\r\n"))) return string();
   else if (matchStart(str, header)) {
