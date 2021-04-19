3bd29022aa1e57f101029cda40e46e4141af6da9
Yanjun Jin <jin327@data.cs.purdue.edu>
2021-04-18 23:23:34 -0400

    Commit

--- myhttpd.cc
+++ myhttpd.cc
@@ -77,7 +77,8 @@ void intHandler(int signum) {
       wait(NULL);
       break;
     case POOL_OF_THREADS:
-      for (int i = 0; i < 5; i++) pthread_cancel(thread[i]);
+      pthread_exit(0);
+      //for (int i = 0; i < 5; i++) pthread_cancel(thread[i]);
       break;
   }
   close(serverSocket);
